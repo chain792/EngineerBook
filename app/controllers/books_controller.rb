6c401eb37270a1ac082d9ca24eb1a3d3449803bf
itoshusu <sopfantasy@yahoo.co.jp>
2022-05-07 08:34:18 +0900

    add: 本検索機能追加

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,8 +1,13 @@
 class BooksController < ApplicationController
+  def index
+
+  end
   def search
-    url = "https://www.googleapis.com/books/v1/volumes"
-    text='プログラミング'
-    res = Faraday.get(url, q: text)
-    @books = JSON.parse(res.body)
+    if params[:search]
+      url = "https://www.googleapis.com/books/v1/volumes"
+      text = params[:search]
+      res = Faraday.get(url, q: text, maxResults: 30)
+      @books = JSON.parse(res.body)
+    end
   end
 end
