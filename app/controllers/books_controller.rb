fd236cfa34f0e27c6a5ee968d20988e43e370639
itoshusu <sopfantasy@yahoo.co.jp>
2022-05-09 23:34:04 +0900

    add: レビュー編集機能追加

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,5 +1,5 @@
 class BooksController < ApplicationController
-  before_action :set_volume_info, only: %i[create]
+  before_action :set_book, only: %i[edit update destroy]
 
   def index
     @books = Book.all.includes(:authors,:user).order(created_at: :desc)
@@ -15,6 +15,7 @@ class BooksController < ApplicationController
     if @book.save_with_author(authors_params[:authors])
       redirect_to books_path, success: t('defaults.message.created', item: t('defaults.review'))
     else
+      set_volume_info
       flash.now[:danger] = t('defaults.message.not_created', item: t('defaults.review'))
       render 'new'
     end
@@ -24,16 +25,18 @@ class BooksController < ApplicationController
     @book = Book.find(params[:id])
   end
 
-  def edit
-
-  end
+  def edit; end
 
   def update
-
+    if @book.update(book_params)
+      redirect_to book_path(@book), success: t('defaults.message.updated', item: t('defaults.review'))
+    else
+      flash.now[:danger] = t('defaults.message.not_updated', item: t('defaults.review'))
+      render 'edit'
+    end
   end
 
   def destroy
-    @book = current_user.books.find(params[:id])
     @book.destroy!
     redirect_to books_path, success: t('defaults.message.deleted', item: t('defaults.review'))
   end
@@ -50,11 +53,20 @@ class BooksController < ApplicationController
   private
 
   def book_params
-    params.require(:book).permit(:title, :body, :remote_book_image_url, :info_link, :published_date)
+    case action_name
+    when 'create'
+      params.require(:book).permit(:title, :body, :remote_book_image_url, :info_link, :published_date)
+    when 'update'
+      params.require(:book).permit(:body)
+    end
   end
 
   def authors_params
     params.require(:book).permit(authors: [])
+  end
+
+  def set_book
+    @book = current_user.books.find(params[:id])
   end
 
   def set_volume_info
