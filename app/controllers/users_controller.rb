3a4eb8b618fa9ee4f1e72c8bdc05bdde1cbc38e6
itoshusu <sopfantasy@yahoo.co.jp>
2022-05-23 07:18:05 +0900

    add: 非ログイン時閲覧できるページの調整

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,5 @@
 class UsersController < ApplicationController
-  skip_before_action :require_login, only: %i[index new create]
+  skip_before_action :require_login, only: %i[index new create show following follower]
   before_action :set_user, only: %i[show following follower]
 
   def index
