bb1a123d7287d092cdb95e82a7ccdf2de15469ef
itoshusu <sopfantasy@yahoo.co.jp>
2022-06-12 14:30:32 +0900

    fix: sitemapをs3にアップするアダプターはfog_awsを使用する

--- config/sitemap.rb
+++ config/sitemap.rb
@@ -3,9 +3,10 @@ SitemapGenerator::Sitemap.default_host = "https://engineer-book.herokuapp.com"
 SitemapGenerator::Sitemap.sitemaps_path = 'sitemaps/'
 SitemapGenerator::Sitemap.sitemaps_host = "https://s3-ap-northeast-1.amazonaws.com/#{ENV['AWS_S3_BUCKET_NAME']}/"
 
-SitemapGenerator::Sitemap.adapter = SitemapGenerator::AwsSdkAdapter.new(
-  ENV['AWS_S3_BUCKET_NAME'],
-  aws_region: 'ap-northeast-1',
+SitemapGenerator::Sitemap.adapter = SitemapGenerator::S3Adapter.new(
+  fog_provider: 'AWS',
+  fog_directory: ENV['AWS_S3_BUCKET_NAME'],
+  fog_region: 'ap-northeast-1',
   aws_access_key_id: ENV['AWS_ACCESS_KEY_ID'],
   aws_secret_access_key: ENV['AWS_SECRET_ACCESS_KEY']
 )
